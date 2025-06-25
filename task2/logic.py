787b50f1c79274bdeb4a57f68d6359e66f720bf3
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2025-06-26 01:56:03 +0200

    Completed implementation of logic for task 2, passed every test

--- task2/logic.py
+++ task2/logic.py
@@ -1,5 +1,19 @@
-from typing import Any
+from typing import Any, Dict
 
 
-def lookup() -> Any:
-    pass
+def lookup(obj: Dict, path: str) -> Any:
+    try:
+        for key in path.split("."):
+            if not isinstance(obj, Dict):
+                return None
+
+            if key in obj:
+                obj = obj[key]
+            else:
+                try:
+                    obj = obj[int(key)]
+                except(ValueError, KeyError):
+                    return None
+        return obj
+    except(KeyError, TypeError):
+        return None
